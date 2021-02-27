c36488ac759737710e2d2bda0be3bc07db39a5d7
tosyaliselim <selimtosyali@protonmail.com>
2021-02-27 21:55:15 +0300

    Initial project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # spring-todo-list
+
 To Do List web application using Spring Boot
---
+++ src/main/java/com/selimtosyali/springtodolist/SpringTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.selimtosyali.springtodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTodoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTodoListApplication.class, args);
+    }
+
+}
